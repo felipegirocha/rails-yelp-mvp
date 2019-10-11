35991770313c912c7ea89f2d777c8b1fef661503
Felipe Rocha <felipegirocha@gmail.com>
2019-10-10 21:33:31 -0300

    Yelp MVP

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,16 +1,28 @@
 class RestaurantsController < ApplicationController
   def index
-    # @restaurants = Restaurant.all
-  end
-
-  def show
-    # @restaurant = Restaurant.find(params[:restaurant_id])
-
-  end
-
-  def new
+    @restaurants = Restaurant.all
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number, :completed)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name: 'Dishoom',
+    address: '7 Boundary St, London E2 7JE',
+    phone_number: '+5511970321415',
+    category: 'japanese'
+  },
+  {
+    name: 'Pizza East',
+    address: '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '+5511837268492',
+    category: 'chinese'
+  },
+  {
+    name: 'McDonalds',
+    address: 'Av. Brasil s/numero',
+    phone_number: '+5511666666',
+    category: 'french'
+  },
+  {
+    name: 'Cantina Italiana',
+    address: 'Rua treze de maio, 3208',
+    phone_number: '+551132081629',
+    category: 'belgian'
+  },
+  {
+    name: 'BurgerKing',
+    address: 'Rua pamplona 2398',
+    phone_number: '+5511pressaoarterial',
+    category: 'french'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
